0f629fa329328ffa58ef4d622fd5fcee2b1af9cc
Mukarram Haq <mukarramhaq@mukarrams-air.mynetworksettings.com>
2023-12-30 13:50:08 -0500

    Updated Book Entity

--- src/main/java/com/bookStore/VirtualBookStore/Entity/Book.java
+++ src/main/java/com/bookStore/VirtualBookStore/Entity/Book.java
@@ -1,4 +1,18 @@
 package com.bookStore.VirtualBookStore.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String bookName;
+    private String authorName;
+    private String description;
+
+
 }
